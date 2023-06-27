ccde65d65269b65b308f686caffbc402c1e9470f
Gabriel <gabrieldossantosleite@gmail.com>
2023-06-27 17:53:27 -0300

    atualizacoes

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -1,22 +1,28 @@
-    <?php
-        switch(@$_REQUEST["acao"]){
+<?php
+    include("config.php");
+    switch(@$_REQUEST["acao"]){
 
-            case 'cadastrar';
-                $nome = $_POST["nome"];
-                $email = $_POST["email"];
-                $senha = $_POST["senha"];
-                $data_nasc = $_POST["data_nasc"];
+        case 'cadastrar';
+            $nome = filter_input(INPUT_POST, 'name');
+            $email = filter_input(INPUT_POST, 'email' FILTER_VALIDATE_EMAIL);
+            $senha = filter_input(INPUT_POST, 'senha');
+            $data_nasc = filter_input(INPUT_POST, 'data_nasc');
 
-                $sql = "INSERT INTO usuario (nome, email, senha, data_nasc) VALUES ('{$nome}', '{$email}', '{$senha}', '{$data_nasc}');";
-                $res = $conn -> querry($sql);
+            $sql = $pdo->prepare("INSERT INTO usuario (nome, email, senha, data_nasc) VALUES ('$name', '$email', '$senha', '$data_nasc')");
+            $sql->execute();
 
+            header("Location: index.php");
+            exit;
+
+
+        
             break;
 
-            case 'editar';
-            break;
+        case 'editar';
+        break;
 
-            case 'excluir';
-            break;
-        }
+        case 'excluir';
+        break;
+    }
 
-    ?>
+?>
